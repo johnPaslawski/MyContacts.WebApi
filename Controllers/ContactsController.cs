87d2bfd34473b73d7f7bcd5db0ae9e906259d1c0
johnPaslawski <jpaslawski93@gmail.com>
2021-07-19 16:06:39 +0200

    Implemented: GetContacts and GetContact

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -13,14 +13,14 @@ namespace MyContacts.WebApi.Controllers
     public class ContactsController : ControllerBase
     {
         //GET http://localhost:33333/api/contacts
-        // dekorator zakomentowany, bo jest już w 12 linijce do całego kontrolera
+        // dekorator zakomentowany, bo api/contacts jest już w 12 linijce do całego kontrolera
         //[HttpGet("api/contacts")]
         [HttpGet]
         public IActionResult GetContacts()
         {
-            var contacts = DataService.Current.Contacts;
-            return Ok(contacts);
-            
+            var contactsDto = DataService.Current.Contacts;
+            return Ok(contactsDto);
+
             //return new JsonResult(
             //    new List<object>()
             //    {
@@ -28,6 +28,21 @@ namespace MyContacts.WebApi.Controllers
             //        new { Id = 1, FirstName = "Anna", LastName = "Nowak", Email = "anowak@gmail.com"}
             //    }
             //);
+        }
+
+        //GET http://localhost:33333/api/contacts/1
+        [HttpGet("{id:int}")]
+        public IActionResult GetContact(int id)
+        {
+            var contactDto = DataService.Current.Contacts.FirstOrDefault(x => x.Id == id);
+
+            // żeby nie dostać response 206 czyli noContent, tylko 404 że nie znaleziono i że jest lipa
+            if (contactDto == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contactDto);
         }
     }
 }
