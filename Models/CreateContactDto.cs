a1ee28f34036c7da5a7525e92e323b4bd6297771
johnPaslawski <jpaslawski93@gmail.com>
2021-07-19 16:38:38 +0200

    Implemented: CreateContact

---
+++ Models/CreateContactDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyContacts.WebApi.Models
+{
+    public class CreateContactDto
+    {
+        //ID chce generowac automatycznie, a Name nie chcę ustawiać - to różnice w stosunku do ContactDto
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
